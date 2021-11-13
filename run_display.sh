e87d9524a2937235d9827d5cb0638fd9cbd6b337
cmdcheshire <39660009+cmdcheshire@users.noreply.github.com>
2021-11-13 12:52:41 -0800

    changed scripts to forever

--- run_display.sh
+++ run_display.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-(cd /home/pi/pi_studio_display; node update_calendar.js) &
+(cd /home/pi/pi_studio_display; forever start update_calendar.js) &
 echo 'update_calendar started'
-(cd /home/pi/pi_studio_display; node calendar_server.js) &
+(cd /home/pi/pi_studio_display; forever start calendar_server.js) &
 echo 'calendar_server started'
 (cd /home/pi/pi_studio_display; http-server html_display) &
 echo 'http server started'
