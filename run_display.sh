02af237d141bf6167e8ad0fb4c94097baf995580
cmdcheshire <39660009+cmdcheshire@users.noreply.github.com>
2021-11-13 12:40:23 -0800

    updated startup script

--- run_display.sh
+++ run_display.sh
@@ -6,5 +6,5 @@ echo 'update_calendar started'
 echo 'calendar_server started'
 (cd /home/pi/pi_studio_display; http-server html_display) &
 echo 'http server started'
-(cd /home/pi/pi_studio_display; chromium localhost:8080) &
+(cd /home/pi/pi_studio_display; chromium --kiosk http://localhost:8080) &
 echo 'browser started on localhost 8080'
